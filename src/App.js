bfaf942627bedabce76953a923e6d3f9c15ca5ef
Lena abu hatab <lenaabuhatab@gmail.com>
2019-06-30 09:20:17 +0300

    added to heroku

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import * as redux from "redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import * as actions from "./actions";
+
+import Header from "./components/shared/Header";
+
+import { OpportunityList } from "./pages/OpportunityList";
+import { Footer } from "./components/shared/Footer";
+import "./App.css";
+
+import PostDetials from "./components/post/post-detials/PostDetials";
+const store = require("./reducers").init();
+class App extends Component {
+  // renderItems() {
+  //   return this.state.items.map((item, index) => {
+  //     return <PostCard key={index} item={item} />;
+  //   });
+  // }
+  render() {
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={OpportunityList} />
+            <Route exact path="/opportunity/:id" component={PostDetials} />
+            <Footer />
+          </div>
+        </BrowserRouter>
+      </Provider>
+    );
+  }
 }
-
 export default App;
